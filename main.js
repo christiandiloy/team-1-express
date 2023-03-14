6636d3a68801edea64835a99d2302bb230f598ae
christiandiloy <diloychristian@gmail.com>
2023-03-14 20:08:58 +0800

    Changed params from itemID to page_name

--- main.js
+++ main.js
@@ -186,10 +186,34 @@ app.get("/keyword", function (req, res) {
   );
 });
 
-app.get("/store/item-page/:itemId", async (req, res) => {
+// app.get("/store/item-page/:itemId", async (req, res) => {
+//   try {
+//     const itemId = req.params.itemId;
+//     const item = await itemModel.findByPk(itemId);
+//     if (item) {
+//       const itemData = {
+//         item_id: item.item_id,
+//         item_name: item.item_name,
+//         item_price: item.item_price,
+//         item_desc: item.item_desc,
+//         item_category: item.item_category,
+//         item_series: item.item_series,
+//         item_alias: item.item_alias
+//       };
+//       res.json(itemData);
+//     } else {
+//       res.status(404).json({ error: "Item not found" });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ error: "Server error" });
+//   }
+// });
+
+app.get("/store/item-page/:pageName", async (req, res) => {
   try {
-    const itemId = req.params.itemId;
-    const item = await itemModel.findByPk(itemId);
+    const pageName = req.params.pageName;
+    const item = await itemModel.findOne({ where: {page_name: pageName}});
     if (item) {
       const itemData = {
         item_id: item.item_id,
@@ -198,7 +222,7 @@ app.get("/store/item-page/:itemId", async (req, res) => {
         item_desc: item.item_desc,
         item_category: item.item_category,
         item_series: item.item_series,
-        item_main_image: item.item_main_image
+        page_name: item.page_name
       };
       res.json(itemData);
     } else {
@@ -209,6 +233,7 @@ app.get("/store/item-page/:itemId", async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 });
+
 
 app.post('/subscribe', async (req, res) => {
 
